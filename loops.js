6958744e6abd5ad92568e49b5d34b644567e6156
Kuman13 <kyym13@gmail.com>
2023-09-03 22:14:21 +0200

    added_for_break

--- loops.js
+++ loops.js
@@ -15,6 +15,12 @@ let num = 10
 // } while (num <= 7) 
 
 
-for (let i = 0; i <= 7; i+2) {
+let sumOfLoop = 0
+
+for (let i = 0; i <= num; i+=2) {
     console.log(i)
+    if (i == 6) break
+    sumOfLoop += i
 }
+
+console.log(sumOfLoop)
