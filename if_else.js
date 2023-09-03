e867a9ce1bd37038699a9eaef8f098c6d352d290
Kuman13 <kyym13@gmail.com>
2023-09-03 20:12:02 +0200

    added_else_clause

--- if_else.js
+++ if_else.js
@@ -2,4 +2,16 @@ let num215 = 215
 
 if (num215 == 215) {
     console.log('yes. value under condition')
+    console.log('1')
+    console.log('2')
+}
+
+if (num215 >= 215 && num215 < 250) {
+    console.log('Second condition works')
+} 
+
+if (num215 > 300) {
+    console.log('num215 variable greater than 300')
+} else {
+    console.log('code goes to the else')
 }
