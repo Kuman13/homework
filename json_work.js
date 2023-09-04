8472532511710e99cf2c924bcd730faea39b3a79
Kuman13 <kyym13@gmail.com>
2023-09-04 17:38:00 +0200

    added_changes_npm_instal

---
+++ json_work.js
@@ -0,0 +1,15 @@
+const values = require('./values.json')
+
+// let jsonObj1 = '{"name":"oleg","lname":"kyym","age":100500}'
+
+// let obj2 = JSON.parse(jsonObj1)
+
+// console.log(obj2.name)
+
+// obj2.city = "York"
+
+// console.log(obj2)
+
+// jsonObj1 = JSON.stringify(obj2)
+
+ console.log(values)
