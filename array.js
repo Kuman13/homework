8d00b86c64c52fc6675c5eef6e4387281ec1df23
Kuman13 <kyym13@gmail.com>
2023-09-07 18:06:11 +0200

    added_new_file

---
+++ array.js
@@ -0,0 +1,45 @@
+
+ //             0     1    2     3        4    5       6
+let arr1 = ['kil', 'ooo', 343, 'jdujd', 2390, true, function() {console.log('Hi from array')}]
+
+
+// let value = arr1[3]
+// console.log(value)
+
+arr1[3] = 'New'
+
+// console.log(arr1[3])
+// console.log(arr1)
+
+// arr1[6]
+
+// let lArr1 = arr1.length
+// console.log(lArr1)
+
+// console.log(arr1.at(-3))
+async function getOnlyString(baseArr) {
+let result = [];
+resultWithUpperC = [];//con
+
+for (let i = 0; i < baseArr.length; i++) {
+    //console.log(arr1[i])
+
+
+    if (typeof arr1[i] === 'string') {
+    await result.push(baseArr[i])
+    }
+}
+    for (let y = 0; y < result.length; y++) {
+
+    let word = result.at(y)
+    let firstLetter = await String(word[0]).toUpperCase()
+    //console.log(firstLetter)
+     let restLetters = await String(word.slice(1))
+    await resultWithUpperC.push(firstLetter + restLetters)
+}
+console.log(resultWithUpperC)
+}
+getOnlyString(arr1)
+
+
+//console.log(resultWithUpperC)
